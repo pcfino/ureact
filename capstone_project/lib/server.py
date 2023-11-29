c0844129104027bdd7d0b5aa03408a3109901733
Eliottnibley <e.nibley@gmail.com>
2023-11-29 09:53:21 -0700

    fix request to be post and grab data

--- capstone_project/lib/server.py
+++ capstone_project/lib/server.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy import butter, filtfilt, find_peaks
+from scipy import signal
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -32,22 +32,27 @@ def zero():
     json_file['counter'] = 0
     return jsonify(json_file)
 
-@app.route('/timeToStability')
-def timeToStability(DataAcc, DataRot, Fs):
-    accNorm = np.linalg.norm(DataAcc, axis=1)
-    rotNorm = np.linalg.norm(DataRot, axis=1)
+@app.route('/timeToStability', methods=['POST'])
+def timeToStability():
+    dataAcc = request.json.get('dataAcc')
+    dataRot = request.json.get('dataRot')
+    fs = request.json.get('fs')
 
-    b, a = butter(2, 10 / (Fs / 2))
+    accNorm = np.linalg.norm(dataAcc, axis=1)
+    rotNorm = np.linalg.norm(dataRot, axis=1)
+
+
+    b, a = signal.butter(2, 10 / (fs / 2))
 
     #Foot Movement: 9.81*1.07; % based on El-Gohary Threshold for foot movement
-    qaF = filtfilt(b, a, accNorm) < 9.81*1.07
+    qaF = signal.filtfilt(b, a, accNorm) < 9.81*1.07
     #Rotational Foot Movement: 14/180*pi; % based on El-Gohary Threshold for foot movement* this should be 7 deg/sec?
-    qrf = filtfilt(b, a, rotNorm) < 14/180*np.pi
+    qrf = signal.filtfilt(b, a, rotNorm) < 14/180*np.pi
     qf = np.logical_and(qaF, qrf)
 
     #find t0
-    peaks, _ = find_peaks(np.flip(accNorm)[Fs * 3:], height=5)
-    movementF = Fs * 3 + peaks[-1]
+    peaks, _ = signal.find_peaks(np.flip(accNorm)[fs * 3:], height=5)
+    movementF = fs * 3 + peaks[-1]
     release = np.argmax(qf[movementF:]) #reverse of qf? reverse done in movementF?
     release = movementF+release
     t0 = len(accNorm)-release
@@ -57,7 +62,7 @@ def timeToStability(DataAcc, DataRot, Fs):
     EndTTS = np.argmax(qf[movementReg:])
     EndTTS = EndTTS+movementReg
 
-    TTS = (EndTTS - t0)/Fs
+    TTS = (EndTTS - t0)/fs
 
     return jsonify({'t0': t0, 'EndTTS': EndTTS, 'TTS': TTS})
 

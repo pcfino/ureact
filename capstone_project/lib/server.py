1133bcc29664700bf35453e79ae0c1d107c12247
Eliottnibley <e.nibley@gmail.com>
2023-11-29 09:03:04 -0700

    copy paste script, move refresh rate to input of the function, and jsonify the result for the return

--- capstone_project/lib/server.py
+++ capstone_project/lib/server.py
@@ -1,3 +1,5 @@
+import numpy as np
+from scipy import butter, filtfilt, find_peaks
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -29,6 +31,35 @@ def increment():
 def zero():
     json_file['counter'] = 0
     return jsonify(json_file)
+
+@app.route('/timeToStability')
+def timeToStability(DataAcc, DataRot, Fs):
+    accNorm = np.linalg.norm(DataAcc, axis=1)
+    rotNorm = np.linalg.norm(DataRot, axis=1)
+
+    b, a = butter(2, 10 / (Fs / 2))
+
+    #Foot Movement: 9.81*1.07; % based on El-Gohary Threshold for foot movement
+    qaF = filtfilt(b, a, accNorm) < 9.81*1.07
+    #Rotational Foot Movement: 14/180*pi; % based on El-Gohary Threshold for foot movement* this should be 7 deg/sec?
+    qrf = filtfilt(b, a, rotNorm) < 14/180*np.pi
+    qf = np.logical_and(qaF, qrf)
+
+    #find t0
+    peaks, _ = find_peaks(np.flip(accNorm)[Fs * 3:], height=5)
+    movementF = Fs * 3 + peaks[-1]
+    release = np.argmax(qf[movementF:]) #reverse of qf? reverse done in movementF?
+    release = movementF+release
+    t0 = len(accNorm)-release
+
+    #find TTS
+    movementReg = len(accNorm)-movementF
+    EndTTS = np.argmax(qf[movementReg:])
+    EndTTS = EndTTS+movementReg
+
+    TTS = (EndTTS - t0)/Fs
+
+    return jsonify({'t0': t0, 'EndTTS': EndTTS, 'TTS': TTS})
 
 # Run the developement server
 if __name__ == '__main__':

ef7ac46d3447bd34e0176a7d3c19824691f398f4
timlocott <tsam.cottrell@gmail.com>
2023-12-05 16:39:18 -0700

    Added profile fields and incident list

--- capstone_project/lib/main.dart
+++ capstone_project/lib/main.dart
@@ -174,23 +174,194 @@ class PatientPage extends StatelessWidget {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Patient'),
+          title: const Text(''),
           centerTitle: true,
           leading: BackButton(onPressed: () {
             Navigator.pop(context);
           }),
         ),
         body: Center(
-          child: ElevatedButton(
-            child: const Text('Incident'),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const IncidentPage(),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                      flex: 5,
+                      child: Icon(
+                        Icons.account_circle_outlined,
+                        color: Colors.black,
+                        size: 100.0,
+                      ),
+                    ),
+                    Expanded(
+                      flex: 7,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Abby Smith'),
+                          Text('DOB: 10/26/1995'),
+                          Text('Sport: Soccer'),
+                          Text('3rd Party ID: 1234567')
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-              );
-            },
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Expanded(
+                        flex: 1,
+                        child: Text(
+                          'Ht: 5\' 7"',
+                          textAlign: TextAlign.center,
+                        )),
+                    Expanded(
+                        flex: 1,
+                        child: Text(
+                          'Wt: 165 lbs',
+                          textAlign: TextAlign.center,
+                        )),
+                    Expanded(
+                        flex: 1,
+                        child: Text(
+                          'Gender: F',
+                          textAlign: TextAlign.center,
+                        )),
+                  ],
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 8.0),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: Text('Incidents',
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold)),
+                      ),
+                    ],
+                  ),
+                ),
+                const Divider(
+                  height: 1,
+                  thickness: 1,
+                  color: Colors.black,
+                ),
+                Expanded(
+                  child: ListView(
+                    children: ListTile.divideTiles(
+                      context: context,
+                      tiles: [
+                        ListTile(
+                          title: Text('Soccer' + ' - ' + '05/03/2023'),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const IncidentPage(),
+                              ),
+                            );
+                          },
+                        ),
+                        ListTile(
+                          title: Text('Other' + ' - ' + '08/16/2022'),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const IncidentPage(),
+                              ),
+                            );
+                          },
+                        ),
+                        ListTile(
+                          title: Text('Soccer' + ' - ' + '06/15/2021'),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const IncidentPage(),
+                              ),
+                            );
+                          },
+                        ),
+                        ListTile(
+                          title: Text('Soccer' + ' - ' + '04/29/2021'),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const IncidentPage(),
+                              ),
+                            );
+                          },
+                        ),
+                        ListTile(
+                          title: Text('Soccer' + ' - ' + '04/12/2021'),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const IncidentPage(),
+                              ),
+                            );
+                          },
+                        ),
+                        ListTile(
+                          title: Text('Other' + ' - ' + '12/20/2020'),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const IncidentPage(),
+                              ),
+                            );
+                          },
+                        ),
+                        ListTile(
+                          title: Text('Soccer' + ' - ' + '11/14/2020'),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const IncidentPage(),
+                              ),
+                            );
+                          },
+                        ),
+                        ListTile(
+                          title: Text('Soccer' + ' - ' + '10/12/2020'),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const IncidentPage(),
+                              ),
+                            );
+                          },
+                        ),
+                        ListTile(
+                          title: Text('Basketball' + ' - ' + '9/12/2020'),
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => const IncidentPage(),
+                              ),
+                            );
+                          },
+                        ),
+                      ],
+                    ).toList(),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
         bottomNavigationBar: BottomAppBar(

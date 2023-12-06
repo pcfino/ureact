d276d4076f808cd10f04742e87cc55a10ebacbfa
cdrudd120 <cdrudd120@gmail.com>
2023-12-06 15:54:32 -0700

    Updated the Incident page

--- capstone_project/lib/main.dart
+++ capstone_project/lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:capstone_project/patient.dart';
+import 'package:capstone_project/patient_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
@@ -160,7 +160,7 @@ class _MyApp extends State<MyApp> {
                         color: Colors.white10,
                         shape: const Border(
                           bottom: BorderSide(
-                            color: Colors.black,
+                            color: Colors.grey,
                           ),
                         ),
                         child: Padding(
@@ -447,11 +447,19 @@ class PatientPage extends StatelessWidget {
   }
 }
 
-class IncidentPage extends StatelessWidget {
+class IncidentPage extends StatefulWidget {
   const IncidentPage({super.key});
 
   @override
+  State<IncidentPage> createState() => _IncidentPage();
+}
+
+class _IncidentPage extends State<IncidentPage> {
+  TextEditingController _date = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
+    _date.text = "2023/5/3";
     return MaterialApp(
       title: 'Incident',
       theme: ThemeData(
@@ -466,20 +474,134 @@ class IncidentPage extends StatelessWidget {
             Navigator.pop(context);
           }),
         ),
-        body: Center(
-          child: ElevatedButton(
-            child: const Text('Run Test'),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) =>
-                      const StartTestPage(title: 'Reactive Test'),
-                ),
-              );
-            },
+        body: Padding(
+          padding: const EdgeInsets.all(16),
+          child: SingleChildScrollView(
+            child: ConstrainedBox(
+              constraints: const BoxConstraints(
+                minHeight: 80,
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                        ),
+                        labelText: "Name",
+                        contentPadding: EdgeInsets.all(11),
+                      ),
+                      controller: TextEditingController(
+                        text: "Soccer",
+                      ),
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(0, 0, 0, 5),
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                        ),
+                        labelText: "Date",
+                        contentPadding: EdgeInsets.all(11),
+                      ),
+                      controller: _date,
+                      onTap: () async {
+                        DateTime? selectedDate = await showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime.now(),
+                        );
+                        if (selectedDate != null) {
+                          setState(() {
+                            _date.text =
+                                "${selectedDate.year}/${selectedDate.month}/${selectedDate.day}";
+                          });
+                        }
+                      },
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(0, 0, 0, 5),
+                    child: TextField(
+                      maxLines: null,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: "Notes",
+                      ),
+                      controller: TextEditingController(
+                        text:
+                            "Abby was hit in the head by another player while playing a soccer game and experienced a concussion.",
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                    child: const Text(
+                      'Tests',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: NeverScrollableScrollPhysics(),
+                    itemCount: 15,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                        margin: const EdgeInsets.all(0),
+                        elevation: 0,
+                        color: Colors.white10,
+                        shape: const Border(
+                          top: BorderSide(
+                            color: Colors.grey,
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "2023/${index * 2}/$index",
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
           ),
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) =>
+                    const StartTestPage(title: 'Reactive Test'),
+              ),
+            );
+          },
+          child: const Icon(Icons.add),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endContained,
       ),
     );
   }

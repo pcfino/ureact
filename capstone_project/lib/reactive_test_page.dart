1711e076fd027fdf48c10652b94f994ec0499daf
GyabiG <gabbyagoodman@yahoo.com>
2024-03-29 11:36:02 -0600

    Cleaned Up meeting code, added method comments.

--- capstone_project/lib/reactive_test_page.dart
+++ capstone_project/lib/reactive_test_page.dart
@@ -90,7 +90,7 @@ class _ReactiveTestPage extends State<ReactiveTestPage> {
     SensorRecorderResults? sensorData;
     try {
       sensorData = sensorRecorder.endRecording();
-      print(sensorData.accData.x.length);
+      //print(sensorData.accData.x.length);
       var decodedData = await runReactiveTestScript({
         'dataAcc': sensorData.formattedAccData(),
         'dataRot': sensorData.formattedGyrData(),
@@ -99,6 +99,7 @@ class _ReactiveTestPage extends State<ReactiveTestPage> {
       });
 
       timeToStab = decodedData['TTS'];
+      //Output to Consol for testing purposes TODO: REMOVE LATER
       print(timeToStab);
       nextTest();
     } catch (e) {

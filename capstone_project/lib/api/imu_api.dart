cc037ad7b7ddca42de8d29c06a55c4e62877b531
jarrodking35 <jarrodking23@outlook.com>
2024-04-07 16:00:55 -0600

    api calls for server IMU DATA

---
+++ capstone_project/lib/api/imu_api.dart
@@ -0,0 +1,38 @@
+import 'api_util.dart' as api;
+import 'dart:convert';
+
+/// get IMU data from the database
+///
+/// @param patientId: id of the patient
+///
+/// @return Json object with the IMU Data
+Future getIMU(int? rID, int? sID, int? dID) async {
+  String enpointString = "/mysql/getIMU?";
+  if (rID != null) {
+    enpointString += "rID=$rID";
+    if (sID != null || dID != null) {
+      enpointString += "&";
+    }
+  }
+  if (sID != null) {
+    enpointString += "sID=$sID";
+    if (dID != null) {
+      enpointString += "&";
+    }
+  }
+  if (dID != null) {
+    enpointString += "dID=$dID";
+  }
+  var results = await api.get(enpointString);
+  return await jsonDecode(results);
+}
+
+/// insert IMU data to the database
+///
+/// @param patientId: id of the patient
+///
+/// @return Json object with the patient infomation
+Future insertIMU(Map imuInfo) async {
+  var results = await api.post('/mysql/insertIMU', imuInfo);
+  return await jsonDecode(results);
+}
